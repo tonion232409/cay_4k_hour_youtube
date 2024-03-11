d04b5753d7517e26e71145b993253fd232288e96
tonion232409 <163017333+tonion232409@users.noreply.github.com>
2024-03-11 12:11:39 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-pip install selenium
-apt-get update
-apt install chromium-chromedriver
-cp /usr/lib/chromium-browser/chromedriver /usr/bin
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.debc &> /dev/null &&
+dpkg -i google-chrome-stable_current_amd64.deb &> /dev/null &&
+pip install selenium &> /dev/null &&
+apt-get update &> /dev/null &&
+apt install chromium-chromedriver &> /dev/null &&
+cp /usr/lib/chromium-browser/chromedriver /usr/bin &> /dev/null &&
 
 python get_list_videos.py
-python cay_4k_view_youtube.py
+python cay_4k_view_youtube.py &
+python cay_4k_view_youtube.py &
+python cay_4k_view_youtube.py &
+
+wait
+echo "Tất cả các công việc đã hoàn thành."
