211e2c5fd5fc6221cd9a0522525dd78787741a3d
tonion232409 <163017333+tonion232409@users.noreply.github.com>
2024-03-11 12:54:40 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.debc &> /dev/null &&
-dpkg -i google-chrome-stable_current_amd64.deb &> /dev/null &&
-pip install selenium &> /dev/null &&
-apt-get update &> /dev/null &&
-apt install chromium-chromedriver &> /dev/null &&
-cp /usr/lib/chromium-browser/chromedriver /usr/bin &> /dev/null &&
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.debc > /dev/null 2>&1
+dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+pip install selenium > /dev/null 2>&1
+apt-get update > /dev/null 2>&1
+apt install chromium-chromedriver > /dev/null 2>&1
+cp /usr/lib/chromium-browser/chromedriver /usr/bin > /dev/null 2>&1
 
 python get_list_videos.py
 python cay_4k_view_youtube.py
