a4a74504d1658e1f72a670e4a0a8df684b69ff66
tonion232409 <163017333+tonion232409@users.noreply.github.com>
2024-03-13 13:13:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.debc > /dev/null 2>&1
 dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 pip install selenium > /dev/null 2>&1
+pip install pytube > /dev/null 2>&1
 apt-get update > /dev/null 2>&1
 apt install chromium-chromedriver > /dev/null 2>&1
 cp /usr/lib/chromium-browser/chromedriver /usr/bin > /dev/null 2>&1
